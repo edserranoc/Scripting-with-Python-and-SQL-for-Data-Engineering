d93898b4a8c282a89323b28c2f9f72e8a6b4ce8c
edserranoc <edison.serrano@cimat.mx>
2025-04-01 16:53:41 -0600

    update notes week 2

--- week2/python_scripts/create.py
+++ week2/python_scripts/create.py
@@ -3,7 +3,7 @@ import sqlite3
 
 # The next thing that you want to do is create a connection. 
 # I'm just going to name it sample.db
-connection = sqlite3.connect('sample.db')
+connection = sqlite3.connect('../data/sample.db')
 
 # I want to create a table
 table = 'CREATE TABLE people (id integer primary key, name TEXT,surname TEXT)'
